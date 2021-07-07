461c8729cb047cdebd95e190288abd80740c0190
DESKTOP-UMTQLOA\Viktor <oviktor92@gmail.com>
2021-07-07 23:20:39 +0200

    -20210707  - Beta - copy AtoB folder

---
+++ CopyAtoB/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace CopyAtoB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(15, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 164);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "\"A\"\r\nmappa tartalma\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(667, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 34);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "\"B\"\r\nmappa tartalma\r\n";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(670, 54);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(300, 164);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 34);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "\"CSV\"\r\nfájl tartalma\r\n";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(344, 54);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(300, 164);
+            this.listBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Másolás B-be CSV szerint";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "CopyDev";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CopyAtoB/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CopyAtoB
+{
+  
+
+    public partial class Form1 : Form
+    {
+        List<string> pdfnames;
+        List<string> csvPdfNames;
+        List<string> copyFiles;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pdfnames = new List<string>(Directory.EnumerateFiles(@"A/"));
+            csvPdfNames = new List<string>(File.ReadAllLines(@"CSVFile/PDFNames.csv", Encoding.UTF8));
+            copyFiles = new List<string>();
+            foreach (string item in pdfnames)
+            {
+                listBox1.Items.Add(item.Substring(2));
+            }
+            foreach (string item in csvPdfNames)
+            {
+                if (item.Count() != 0 && item.Trim().Count() !=0)
+                {
+                    int temp = item.LastIndexOf(';');
+                    listBox3.Items.Add(item.Remove(temp, 1));
+                }
+             
+
+            }
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            foreach (string item in listBox1.Items)
+            {
+                foreach (string item2 in listBox3.Items)
+                {
+                    if (item == item2)
+                    {
+                        copyFiles.Add(item);
+                       
+                    }
+                }
+            }
+            lb2Refresh();
+           copyToB();
+        }
+
+        private void copyToB()
+        {
+            foreach (string item in copyFiles)
+            {
+                File.Copy($"A/{item}", $"B/{item}");
+            }
+        }
+
+        private void lb2Refresh()
+        {
+            listBox2.DataSource = null;
+            listBox2.DataSource = copyFiles;
+        }
+    }
+}
